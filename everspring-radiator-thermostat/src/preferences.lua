ef890f5e097fa667e9829a981d9df1bc2e5675c2
BassPete <120145031+BassPete@users.noreply.github.com>
2022-12-10 00:06:02 +0100

    Update preferences.lua

--- everspring-radiator-thermostat/src/preferences.lua
+++ everspring-radiator-thermostat/src/preferences.lua
@@ -1,6 +1,6 @@
 local preferences = {
   temperatureAutoReport = {parameter_number = 1, size = 4},
-  Openwindowdetect = {parameter_number = 6, size = 1}
+  openWindowDetect = {parameter_number = 6, size = 1}
 }
 
 return preferences
