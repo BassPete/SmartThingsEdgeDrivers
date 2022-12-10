d5f8507e4b4ed405e5d19ceab0adc7ac6b52b53d
BassPete <120145031+BassPete@users.noreply.github.com>
2022-12-10 01:46:55 +0100

    Update init.lua

--- everspring-radiator-thermostat/src/init.lua
+++ everspring-radiator-thermostat/src/init.lua
@@ -20,39 +20,21 @@ local defaults = require "st.zwave.defaults"
 --- @type st.zwave.CommandClass.Battery
 local Battery = (require "st.zwave.CommandClass.Battery")({version=1})
 --- @type st.zwave.CommandClass.SensorMultilevel
-local SensorMultilevel = (require "st.zwave.CommandClass.SensorMultilevel")({version=2})
---- @type st.zwave.CommandClass.ThermostatFanMode
-local ThermostatFanMode = (require "st.zwave.CommandClass.ThermostatFanMode")({version=3})
+local SensorMultilevel = (require "st.zwave.CommandClass.SensorMultilevel")({version=1})
 --- @type st.zwave.CommandClass.ThermostatMode
-local ThermostatMode = (require "st.zwave.CommandClass.ThermostatMode")({version=2})
---- @type st.zwave.CommandClass.ThermostatOperatingState
-local ThermostatOperatingState = (require "st.zwave.CommandClass.ThermostatOperatingState")({version=1})
+local ThermostatMode = (require "st.zwave.CommandClass.ThermostatMode")({version=3})
 --- @type st.zwave.CommandClass.ThermostatSetpoint
-local ThermostatSetpoint = (require "st.zwave.CommandClass.ThermostatSetpoint")({version=1})
+local ThermostatSetpoint = (require "st.zwave.CommandClass.ThermostatSetpoint")({version=3})
 local constants = require "st.zwave.constants"
 local utils = require "st.utils"
 
 local do_refresh = function(self, device)
-  device:send(ThermostatFanMode:SupportedGet({}))
-  device:send(ThermostatFanMode:Get({}))
   device:send(ThermostatMode:SupportedGet({}))
   device:send(ThermostatMode:Get({}))
-  device:send(ThermostatOperatingState:Get({}))
   device:send(SensorMultilevel:Get({}))
-  device:send(ThermostatSetpoint:Get({setpoint_type = ThermostatSetpoint.setpoint_type.COOLING_1}))
+  device:send(ThermostatSetpoint:Get({setpoint_type = ThermostatSetpoint.setpoint_type.ENERGY_1}))
   device:send(ThermostatSetpoint:Get({setpoint_type = ThermostatSetpoint.setpoint_type.HEATING_1}))
   device:send(Battery:Get({}))
-end
-
---TODO: Update this once we've decided how to handle setpoint commands
-local function convert_to_device_temp(command_temp, device_scale)
-  -- under 40, assume celsius
-  if (command_temp < 40 and device_scale == ThermostatSetpoint.scale.FAHRENHEIT) then
-    command_temp = utils.c_to_f(command_temp)
-  elseif (command_temp >= 40 and (device_scale == ThermostatSetpoint.scale.CELSIUS or device_scale == nil)) then
-    command_temp = utils.f_to_c(command_temp)
-  end
-  return command_temp
 end
 
 local function set_setpoint_factory(setpoint_type)
@@ -77,37 +59,26 @@ end
 
 local driver_template = {
   supported_capabilities = {
-    capabilities.temperatureAlarm,
     capabilities.temperatureMeasurement,
     capabilities.thermostatHeatingSetpoint,
-    capabilities.thermostatCoolingSetpoint,
-    capabilities.thermostatOperatingState,
+    capabilities.thermostatEnergySetpoint,
     capabilities.thermostatMode,
-    capabilities.thermostatFanMode,
-    capabilities.relativeHumidityMeasurement,
     capabilities.battery,
     capabilities.powerMeter,
-    capabilities.energyMeter
   },
   capability_handlers = {
     [capabilities.refresh.ID] = {
       [capabilities.refresh.commands.refresh.NAME] = do_refresh
     },
     [capabilities.thermostatCoolingSetpoint.ID] = {
-      [capabilities.thermostatCoolingSetpoint.commands.setCoolingSetpoint.NAME] = set_setpoint_factory(ThermostatSetpoint.setpoint_type.COOLING_1)
+      [capabilities.thermostatCoolingSetpoint.commands.setCoolingSetpoint.NAME] = set_setpoint_factory(ThermostatSetpoint.setpoint_type.ENERGY_1)
     },
     [capabilities.thermostatHeatingSetpoint.ID] = {
       [capabilities.thermostatHeatingSetpoint.commands.setHeatingSetpoint.NAME] = set_setpoint_factory(ThermostatSetpoint.setpoint_type.HEATING_1)
     }
   },
   sub_drivers = {
-    require("aeotec-radiator-thermostat"),
-    require("popp-radiator-thermostat"),
-    require("ct100-thermostat"),
-    require("fibaro-heat-controller"),
-    require("stelpro-ki-thermostat"),
-    require("qubino-flush-thermostat"),
-    require("thermostat-heating-battery")
+    require("everspring-radiator-thermostat"),
   }
 }
 
