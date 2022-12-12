b73c9461b9c8d3dd889cc72f8fe0d32084f76bc5
BassPete <120145031+BassPete@users.noreply.github.com>
2022-12-12 12:09:10 +0100

    Update init.lua

--- everspring-radiator-thermostat/src/init.lua
+++ everspring-radiator-thermostat/src/init.lua
@@ -12,121 +12,87 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+local SensorMultilevel = (require "st.zwave.CommandClass.SensorMultilevel")({ version = 1 })
+local ThermostatSetpoint = (require "st.zwave.CommandClass.ThermostatSetpoint")({ version = 1 })
+local ThermostatMode = (require "st.zwave.CommandClass.ThermostatMode")({ version = 1 })
+local Configuration = (require "st.zwave.CommandClass.Configuration")({ version = 1 })
+local Clock = (require "st.zwave.CommandClass.Clock")({ version = 1 })
 local capabilities = require "st.capabilities"
---- @type st.zwave.Driver
 local ZwaveDriver = require "st.zwave.driver"
---- @type st.zwave.defaults
 local defaults = require "st.zwave.defaults"
---- @type st.zwave.CommandClass.Battery
-local Battery = (require "st.zwave.CommandClass.Battery")({version=1})
---- @type st.zwave.CommandClass.SensorMultilevel
-local SensorMultilevel = (require "st.zwave.CommandClass.SensorMultilevel")({version=1})
---- @type st.zwave.CommandClass.ThermostatMode
-local ThermostatMode = (require "st.zwave.CommandClass.ThermostatMode")({version=3})
---- @type st.zwave.CommandClass.ThermostatSetpoint
-local ThermostatSetpoint = (require "st.zwave.CommandClass.ThermostatSetpoint")({version=3})
---- @type st.zwave.CommandClass.Clock
-local Clock = require "st.zwave.CommandClass.Clock"
+local cc = require "st.zwave.CommandClass"
+local log = require "log"
 local constants = require "st.zwave.constants"
 local utils = require "st.utils"
-
-local LATEST_BATTERY_REPORT_TIMESTAMP = "latest_battery_report_timestamp"
-local LATEST_CLOCK_SET_TIMESTAMP = "latest_clock_set_timestamp"
+local delay_send = require "delay_send"
 local WEEK = {1, 2, 3, 4, 5, 6, 0}
 
-local do_refresh = function(self, device)
-  device:send(ThermostatMode:SupportedGet({}))
-  device:send(ThermostatMode:Get({}))
-  device:send(SensorMultilevel:Get({}))
-  device:send(ThermostatSetpoint:Get({setpoint_type = ThermostatSetpoint.setpoint_type.HEATING_1}))
-  device:send(Battery:Get({}))
-end
-
-local function seconds_since_latest_clock_set(device)
-    local last_clock_set_time = device:get_field(LATEST_CLOCK_SET_TIMESTAMP)
-    if last_clock_set_time ~= nil then
-        return os.difftime(os.time(), last_clock_set_time)
-    end
-    return CLOCK_SET_INTERVAL_SEC + 1
-end
-
-local function check_and_send_battery_get(device)
-    -- Check if time to request new battery report. one time a day
-    if seconds_since_latest_battery_report(device) > BATTERY_REPORT_INTERVAL_SEC then
-        device:send(Battery:Get({}))
-    end
-end
-
-local function set_setpoint_factory(setpoint_type)
-  return function(driver, device, command)
-    local scale = device:get_field(constants.TEMPERATURE_SCALE)
-    local value = convert_to_device_temp(command.args.setpoint, scale)
-
-    local set = ThermostatSetpoint:Set({
-      setpoint_type = setpoint_type,
-      scale = scale,
-      value = value
-    })
-    device:send_to_component(set, command.component)
-
-    local follow_up_poll = function()
-      device:send_to_component(ThermostatSetpoint:Get({setpoint_type = setpoint_type}), command.component)
-    end
-
-    device.thread:call_with_delay(1, follow_up_poll)
-  end
-end
-
-local function cmdClockSet()
-    local now = os.date("*t") -- UTC
-    log.info("ClockSet: ".. now.hour ..":" .. now.min ..":" .. WEEK[now.wday])  -- lua wday starts from Sunday(0).
-    return Clock:Set({hour=now.hour, minute=now.min, weekday=WEEK[now.wday]})
-end
-
-local function check_and_send_clock_set(device)
-    -- Update device clock time, one time a day
-    if seconds_since_latest_clock_set(device) > CLOCK_SET_INTERVAL_SEC then
-        device:send(cmdClockSet())
-        device:set_field(LATEST_CLOCK_SET_TIMESTAMP, os.time())
-    end
-end
-
-local function check_and_send_cached_setpoint(device)
-    local cached_setpoint_command = device:get_field(CACHED_SETPOINT)
-
-    if cached_setpoint_command ~= nil then
-        device:send(cached_setpoint_command)
-        local follow_up_poll = function()
-            device:send(
-                    ThermostatSetpoint:Get({setpoint_type = ThermostatSetpoint.setpoint_type.HEATING_1})
-            )
-        end
-        device.thread:call_with_delay(DELAY_TO_GET_UPDATED_VALUE, follow_up_poll)
-    end
-end
-
-local driver_template = {
-  supported_capabilities = {
-    capabilities.temperatureMeasurement,
-    capabilities.thermostatHeatingSetpoint,
-    capabilities.thermostatMode,
-    capabilities.battery,
-    capabilities.powerMeter,
-  },
-  capability_handlers = {
-    [capabilities.refresh.ID] = {
-      [capabilities.refresh.commands.refresh.NAME] = do_refresh
-    },
-    [capabilities.thermostatHeatingSetpoint.ID] = {
-      [capabilities.thermostatHeatingSetpoint.commands.setHeatingSetpoint.NAME] = set_setpoint_factory(ThermostatSetpoint.setpoint_type.HEATING_1)
-    }
-  },
-  sub_drivers = {
-    require("everspring-radiator-thermostat"),
-  }
+local ZWAVE_THERMOSTAT_FINGERPRINTS = {
+  {mfr = 0x0060, prod = 0x0001, model = 0x0015} -- Everspring AC301
 }
 
+local CONFIG_PARAMS = {}
+
+local function can_handle_zwave_thermostat(opts, driver, device, ...)
+  for _, fingerprint in ipairs(ZWAVE_THERMOSTAT_FINGERPRINTS) do
+    if device:id_match(fingerprint.mfr, fingerprint.prod, fingerprint.model) then
+      return true
+    end
+  end
+  return false
+end
+
+local function refresh(driver,device)
+  log.debug('Refresh')
+  
+  local offset = ((device.preferences or {}).timezoneUTC or 0) + ((device.preferences or {}).timezoneDST or 0)
+  local now = os.date('*t', os.time() + offset * 60 * 60)
+	
+  local cmds = {
+    Clock:Set({ weekday = (now.wday + 5) % 7, hour = now.hour, minute = now.min }),
+    Clock:Get({}),
+    ThermostatFanMode:SupportedGet({}),
+    ThermostatMode:SupportedGet({}),
+    Configuration:Get({ parameter_number = 132 }),  -- Hold = 0, Run Schedule = 1
+    Configuration:Get({ parameter_number = 25 }),   -- Energy save on = 2, off = 0
+    ThermostatFanMode:Get({}),
+    ThermostatMode:Get({}),
+    ThermostatOperatingState:Get({}),
+    ThermostatSetpoint:Get({setpoint_type = ThermostatSetpoint.setpoint_type.HEATING_1}),
+    ThermostatSetpoint:Get({setpoint_type = ThermostatSetpoint.setpoint_type.COOLING_1}),
+    SensorMultilevel:Get({}),
+  }
+  delay_send(device,cmds,1)
+end
+
+local function dev_init(driver, device)
+  log.debug('Device Init')
+  refresh(driver,device)
+end
+
+local function set_heating_setpoint(driver, device, command)
+  local scale = device:get_field(constants.TEMPERATURE_SCALE)
+  local loc_scale = device.state_cache.main.thermostatHeatingSetpoint.heatingSetpoint.unit
+  local value = command.args.setpoint
+  local set = ThermostatSetpoint:Set({
+    setpoint_type = ThermostatSetpoint.setpoint_type.HEATING_1,
+    scale = scale,
+    value = value
+  })
+  device:send_to_component(set, command.component)
+
+  local follow_up_poll = function()
+    device:send_to_component(
+      ThermostatSetpoint:Get({setpoint_type = ThermostatSetpoint.setpoint_type.HEATING_1}),
+      command.component
+    )
+  end
+
+  device.thread:call_with_delay(1, follow_up_poll)
+end
+
 defaults.register_for_default_handlers(driver_template, driver_template.supported_capabilities)
---- @type st.zwave.Driver
-local thermostat = ZwaveDriver("zwave_thermostat", driver_template)
+local thermostat = ZwaveDriver("zwave-thermostat", driver_template)
 thermostat:run()
+  device.thread:call_with_delay(1, follow_up_poll)
+end
